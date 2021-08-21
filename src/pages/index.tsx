03ec17e29a983f6ed659acaa602bf26e0de70972
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2021-08-21 15:35:28 -0300

    feat(🎉): add configs - Fonts and global styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+      </Head>
+      <h1>Oi</h1>
+    </div>
+  );
+}
