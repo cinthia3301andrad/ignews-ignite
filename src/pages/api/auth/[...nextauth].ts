5baad34e10375dd11e70dee0cc0127be9d453aed
Cinthia Andrade <cinthia.andrade@insightlab.ufc.br>
2021-10-10 16:22:48 -0300

    bug: error stripe POST

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -10,5 +14,36 @@ export default NextAuth({
     }),
   ],
 
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),  
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          ),
+        
+        );
+        return true;
+      } catch {
+      
+        return false;
+      }
+    },
+  },
   database: process.env.DATABASE_URL,
 });
